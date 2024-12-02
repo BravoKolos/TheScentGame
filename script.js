1304e0dd79a760a4a57d843f6f2d6fe4f4abed6a
BravoKolos <kamilkolos4321@gmail.com>
2024-12-02 08:32:26 +0100

    starting to add classes for perfume details

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const perfumeCatalog = [
+    {
+        name: "Boss The Scent Elixir",
+        brand: "Hugo Boss",
+        scent: "Spicy, powdery, woody",
+        headNote: "Allspice, Pimento pepper",
+        heartNote: "Lavender, Lavender",
+        baseNote:"Sandalwood",
+        price: 100,
+        volume: "50ml",
+        description: "Highly concentrated, amber and leather composition exudes pure charm thanks to its blend of luminous allspice, sensual sandalwood from New Caledonia and vibrant lavender absolute.",
+        category: "Men's Perfume"
+    }
+];
